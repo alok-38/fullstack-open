7ac66f2080ca672694445b12a426dc785be1ed26
Alokananda Y <plantsfortrees@gmail.com>
2024-12-24 00:24:35 +0000

    Creating a todo list item

--- JavaScript/caleb-curry/script.js
+++
@@ -1,8 +0,0 @@
-const paraEl = document.querySelector("p");
-const buttonEl = document.querySelector("button");
-
-const purchaseConfirmation = () => {
-  paraEl.textContent = "Thank you for your purchase!";
-};
-
-buttonEl.addEventListener("click", purchaseConfirmation);
