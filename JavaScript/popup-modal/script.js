c33eed84ab9189172e4f649326d5cb810fe4a456
Alokananda Y <plantsfortrees@gmail.com>
2024-12-25 02:44:21 +0000

    working on the modal

---
+++ JavaScript/popup-modal/script.js
@@ -0,0 +1,19 @@
+/*
+    TODO: 2. Select the elements with the following IDs
+    * Modal
+    * Open Modal button
+    * Close Modal button
+    * Bonus: overlay
+*/
+
+// TODO: 3. Create a click event listener for the open-modal-btn that adds
+// the class "open" ot the modal
+// Bonus: Also add the class "open" to the overlay
+
+// TODO: 4. Create a click event listener for the close-modal-btn
+// that removes the class "open" from the modal
+// BONUS: Also remove the class "open" from the overlay
+
+// BONUS: Add a click event listener to the overlay that removes
+// class "open" from the  modal and the overlay
+
