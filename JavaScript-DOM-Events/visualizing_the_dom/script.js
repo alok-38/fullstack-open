2116786c0aa9b1ab6e72ab1a3eed91c536d970b8
Alokananda Y <plantsfortrees@gmail.com>
2025-01-22 10:17:05 +0000

    DOM manipulation

---
+++ JavaScript-DOM-Events/visualizing_the_dom/script.js
@@ -0,0 +1,18 @@
+const singleElement = document.querySelector('.example-class');
+const multipleElements = document.querySelectorAll('.highlighted');
+console.log(singleElement, multipleElements);
+singleElement.textContent = "New content for the element";
+multipleElements.forEach((element) => {
+    element.innerHTML = "Updated HTML content";
+    element.style.marginBottom = "1rem";
+});
+singleElement.style.color = "rebeccapurple";
+singleElement.style.fontSize = "1.5em";
+singleElement.style.marginBottom = "1rem";
+
+//  Creating and Adding New Elements
+let newElement = document.createElement("div");
+let paragraph = document.createElement("p");
+paragraph.textContent = "A new paragraph element within a div";
+newElement.appendChild(paragraph);
+document.body.appendChild(newElement);
